8cc6a3fd9d58c4097e4d2c48676cc92279c5d534
shomaken <shomaken14@gmail.com>
2025-07-20 17:58:48 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,12 +9,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: resolve(__dirname, 'index.html'),
+      '@': '/src',
     },
   },
 }) 
