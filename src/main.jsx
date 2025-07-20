084868789770c6a6d1719dc3c562e1aa135b994e
shomaken <shomaken14@gmail.com>
2025-07-20 20:33:23 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Buffer polyfill for Solana/Jupiter libraries
+import { Buffer } from 'buffer'
+window.Buffer = Buffer
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
