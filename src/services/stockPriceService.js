27cfbc08e0b64a3c66468817b0b04f2e61696157
shomaken <shomaken14@gmail.com>
2025-07-22 02:58:07 +0300

    Update stockPriceService.js

--- src/services/stockPriceService.js
+++ src/services/stockPriceService.js
@@ -8,7 +8,7 @@ const DEXSCREENER_API = 'https://api.dexscreener.com/latest/dex/tokens'
 
 // Your actual stock token addresses (with "x" suffix tokens)
 const STOCK_TOKEN_ADDRESSES = {
-  STONKS: 'CwMm1x28qoStmYnsLkRFqQfiQEyBTvKVJjcaSkc2bonk', 
+  STONKS: '27U6sAYSDUJLpeCTTL5gW2wSwLGNRZRZKWJEqTWGbonk', 
   NVDAX: 'Xsc9qvGR1efVDFGLrVsmkzv3qi45LTBjeUKSPmx9qEh',
   TSLAX: 'XsDoVfqeBukxuZHWhdvWHBhgEHjGNst4MLodqsJHzoB', 
   SPYx: 'XsoCS1TfEyfFhfvj8EtZ528L3CaKBDBRqRapnBbDF2W',
